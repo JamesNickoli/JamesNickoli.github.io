7b8809092f474ecad7358e569f8d3fc574f95273
rsninja722 <jnick722547@gmail.com>
2022-03-10 01:13:13 -0500

    better nav bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,17 @@
+function hide() {
+    document.getElementById("hamburger-button").style.display = "";
+    document.getElementById("top-nav").style.display = "none";
+}
 
+function show() {
+    document.getElementById("hamburger-button").style.display = "none";
+    document.getElementById("top-nav").style.display = "";
+}
 
 document.body.onload = function() {
     new Splide( '.splide', {
         width: '640px',
-      }).mount();
+    }).mount();
 }
+
+document.getElementById("hamburger-button").style.display = "none";
