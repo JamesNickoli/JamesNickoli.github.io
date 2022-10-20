e610b20211e34c4b059fa6e38b7e2deb7b0e36c4
rsninja722 <jnick722547@gmail.com>
2022-10-20 03:02:43 -0400

    start experience section and general updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,3 @@
-function hide() {
-    document.getElementById("hamburger-button").style.display = "";
-    document.getElementById("top-nav").style.display = "none";
-}
-
-function show() {
-    document.getElementById("hamburger-button").style.display = "none";
-    document.getElementById("top-nav").style.display = "";
-}
-
 document.body.onload = function() {
     new Splide( '.splide', {
         width: '640px',
