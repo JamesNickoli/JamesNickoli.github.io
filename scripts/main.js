4651283b4488a830ab615f83cba932623eb0c5c8
rsninja722 <jnick722547@gmail.com>
2022-10-21 05:41:03 -0400

    style ready

--- scripts/main.js
+++ scripts/main.js
@@ -42,14 +42,14 @@ document.body.onload = function () {
     document.getElementById("laptop-case").style.transform = "";
 
     let laptopOpen = false;
-    document.getElementById("laptop").classList.add("collapse");
+    document.getElementById("laptop-style").classList.add("collapse");
     document.getElementById("laptop-container").classList.add("laptop-container-move-up");
     document.getElementById("laptop-case").classList.remove("collapse-top-initial");
 
     window.addEventListener("scroll", () => {
             if((window.scrollY - document.getElementById("laptop-container").offsetTop) > 0) {
                 if(!laptopOpen) {
-                    document.getElementById("laptop").classList.remove("collapse");
+                    document.getElementById("laptop-style").classList.remove("collapse");
                     document.getElementById("laptop-case").classList.remove("collapse-top");
                     document.getElementById("laptop-case").classList.add("collapse-top-initial");
                     document.getElementById("laptop-container").classList.remove("laptop-container-move-up");
@@ -57,7 +57,7 @@ document.body.onload = function () {
                 }
             } else {
                 if(laptopOpen) {
-                    document.getElementById("laptop").classList.add("collapse");
+                    document.getElementById("laptop-style").classList.add("collapse");
                     document.getElementById("laptop-case").classList.add("collapse-top");
                     document.getElementById("laptop-case").classList.remove("collapse-top-initial");
                     document.getElementById("laptop-container").classList.add("laptop-container-move-up");
@@ -68,6 +68,65 @@ document.body.onload = function () {
         },
         false
     );
+
+    let canvas = document.getElementById("fancy-background");
+    let ctx = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    let chars = [];
+    let fallingChars = [];
+    let order = "モエヤキオカ7ケサスz152ヨタワ4ネヌナ98ヒ0ホア3ウ セ¦:\"꞊ミラリ╌ツテニハソ▪—<>0|+*コシマムメ";
+    let scale = 40;
+
+    function draw() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        if(canvas.width < 800 ? Math.random() < 0.2 : (canvas.width > 1500 ? Math.random() < 0.7 : Math.random() < 0.4)) {
+            fallingChars.push([random(0, ~~(canvas.width/scale)) * scale, 0]);
+        }
+
+        for(var i of fallingChars) {
+            chars.push([i[0], i[1], 0, order[(~~(i[1]+i[0] / scale)) % order.length]]);
+            i[1] += scale;
+        }
+
+        for(var i=0;i<chars.length;i++) {
+            if(++chars[i][2] > canvas.height/(scale*1.5)) {
+                chars.splice(i, 1);
+                i--;
+                continue;
+            }
+
+            if(Math.random() < 0.1) {
+                chars[i][3] = order[random(0, order.length-1)]
+            }
+        
+        }
+        
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.font = scale + "px monospace";
+        
+        for(var i of chars) {
+            let c = mapRange(i[2], 0, canvas.height/(scale*1.5), 1, 0);
+            ctx.fillStyle  = `rgba(0, 255, 255, ${c})`;
+            ctx.fillText(i[3], i[0], i[1]);
+        }
+    }
+
+    function mapRange( value,  valueLow,  valueHigh,  remappedLow, remappedHigh) {
+        return remappedLow + (remappedHigh - remappedLow) * (value - valueLow) / (valueHigh - valueLow);
+    }
+
+    function random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+
+    setInterval(draw, 100);
 };
 
 function callbackFunc(entries, observer) {
