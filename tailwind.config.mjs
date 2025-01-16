2fa67c6f59e81dddc3b4ac72db1b3e4fec262176
rsninja722 <jnick722547@gmail.com>
2025-01-16 04:04:20 -0500

    Begin Astro Migration

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				back1: "rgb(15, 23, 42)"
+			}
+		},
+	},
+	plugins: [],
+}
