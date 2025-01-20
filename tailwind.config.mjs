d057f10f0cb4c0bb44ab6105537901bbd4797d8a
rsninja722 <jnick722547@gmail.com>
2025-01-20 06:29:16 -0500

    initial content and style complete

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				back1: "rgb(15, 23, 42)"
-			}
-		},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                "t-main": "var(--t-main)",
+                "t-gold": "var(--t-gold)",
+                "t-gold-accent": "var(--t-gold-accent)",
+                "t-main-accent": "var(--t-main-accent)",
+                "t-main-accent-light": "var(--t-main-accent-light)",
+                "t-dark": "var(--t-dark)",
+                "bg-purp": "var(--bg-purp)",
+                "bg-purp-light": "var(--bg-purp-light)",
+                "bg-red": "var(--bg-red)",
+                "bg-bright": "var(--bg-bright)"
+            }
+        },
+    },
+    plugins: [],
 }
